fda8e20f4ccc48f21079b46aafd1cc4a7c002839
aidaimurat <64561162+aidaimurat@users.noreply.github.com>
2021-01-25 21:33:14 +0200

    Add files via upload

---
+++ todo/main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class ToDo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+    
+    
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=100)
+    year = models.DateField()
+
+
+
---
+++ todo/main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo, Book
+
+def homepage(request):
+    return render(request, "index.html")
+    
+def test(request):
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
+
+def books(request):
+    books = Book.objects.all()
+    return render(request, "books.html", {"books": books})
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
+
+def add_book(request):
+    form = request.POST
+    book = Book(
+        title=form["title"],
+        subtitle=form["subtitle"],
+        description=form["description"],
+        price=form["price"],
+        genre=form["genre"],
+        author=form["author"],
+        year=form["year"][:10]
+    )
+    book.save()
+    return redirect(books)
